98bb553a8b6b57e61a1db61842f422111927212a
Gittourarmy <chomyeongsang@gmail.com>
2016-11-19 20:52:50 +0900

    Add files via upload

---
+++ Food.java
@@ -0,0 +1,8 @@
+package io.dionysource.motg.usrclss;
+
+/**
+ * Created by Admin on 2016. 11. 19..
+ */
+
+public class Food {
+}
---
+++ User.java
@@ -0,0 +1,25 @@
+package io.dionysource.motg.usrclss;
+
+import Guest;
+
+/**
+ * Created by Admin on 2016. 11. 19..
+ */
+
+
+class Users{
+    String id;
+    
+
+}
+
+public class Id_usr extends Users{
+
+}
+
+public class Guest_usr extends Users{
+
+    public:
+
+
+} // Local 에다가 식별키를 저장한다.
